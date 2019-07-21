38ce4a371dd9bf84782140e9be2b903462088872
Jason Gama <jasongama967@gmail.com>
2019-07-22 00:55:24 +0200

    done

--- greet-test/test/greet.test.js
+++ greet-test/test/greet.test.js
@@ -1,29 +1,38 @@
 describe('greeting', function() {
-    it("it should greet a person in english", function(){
+    it("It should greet a person first name in english", function(){
         var input = GreetFactory();
-        assert.equal(input.greetInput("English" , "Jason"), "Hello, Jason");
+        input.message("Jason" )
+        input.greetInput("English", "Jason")
+        assert.equal(   input.message( ),"Hello, Jason");
 
 });
-    it("it should greet a person in Isizulu", function(){
+    it("It should greet a person first name in Isizulu", function(){
         var input = GreetFactory();
-        assert.equal(input.greetInput("Isizulu", "Jason"), "Sawubona, Jason");
+        input.message("Joseph")
+        input.greetInput("IsiZulu", "Joseph")
+        assert.equal( input.message(), "Sawubona, Joseph");
          });
-    it("it should greet a person in Afrikaans", function(){
+    it("It should greet a person first name in IsiXhosa", function(){
         var input = GreetFactory();
-        assert.equal(input.greetInput("Jason", "Afrikaans"), "Hallo, Jason");
+        input.message("Thando")
+        input.greetInput("IsiXhosa", "Thando")
+        assert.equal(input.message(), "Molo, Thando");
          });
-         it("it should show alteast three characters", function(){
+         it("It does not take a empty strings", function(){
             var input = GreetFactory();
-            assert.equal(input.greetInput("Jas", "Afrikaans"), "Hallo, Jas");
+            input.message("")
+            input.greetInput()
+            assert.equal(input.message(), "please entere the Name or select your languege");
              });
-             it("should store name Jason ", function(){
+             it("It should make the name Jason an upperCase", function(){
 
-                var instances =  GreetFactory();
-                instances.greetInput("jason")
+                var input =  GreetFactory();
+                input.message("jason")
+                input.greetInput("IsiXhosa", "Jason")
                 
-                assert.deepEqual({name:"Jason"},instances.getGreet())
+                assert.equal( input.message(),"Molo, Jason")
                  });
-                 it("should count the Names", function(){
+                 it("It should count the Names", function(){
 
                     var input = GreetFactory();
                   input.greetInput("English", "Jason");
--- greetLogic.js
+++ greetLogic.js
@@ -6,23 +6,24 @@
 
 
      function greetInput(language, name) {
-         var upperCaseName = name.toUpperCase();
+        
          if(name && language){
+            var upperCaseName = name.toUpperCase();
          if (store[upperCaseName] === undefined) {
              store[upperCaseName] = 0;
         }
          if (language === "English") {
-             theGreeting = "Hello " + name
+             theGreeting = "Hello, " + name
          }
-         if (language === "IsiZulu") {
-             theGreeting = "Sawubona " + name
+         else if (language === "IsiZulu") {
+             theGreeting = "Sawubona, " + name
          }
-         if (language === "IsiXhosa") {
-             theGreeting = "Molo " + name
+         else if (language === "IsiXhosa") {
+             theGreeting = "Molo, " + name
          }
          }
          else{
-            theGreeting = "please entere the Name and select your languege";      
+            theGreeting = "please enter the FirstName or select a languege";      
          }
      }
 
@@ -38,10 +39,7 @@
      function message(){
         return theGreeting
      }  
-     function sayhello(){
-         console.log("hello jason")
-     }
-    
+   
 
      return {
          counter,
