6eda5df8b370835d768a516b3a310fb323cefb0f
jasongama <jasongama967@gmail.com>
2019-07-22 21:43:46 +0200

    done

--- greet-test/test/greet.test.js
+++ greet-test/test/greet.test.js
@@ -1,53 +1,53 @@
-describe('greeting', function() {
-    it("It should greet a person first name in english", function(){
+describe('greeting', function () {
+    it("It should greet my name in english", function () {
         var input = GreetFactory();
-        input.message("jason" )
+        input.message("jason")
         input.greetInput("English", "Jason")
-        assert.equal(   input.message( ),"Hello, Jason");
+        assert.equal(input.message(), "Hello, Jason");
 
-});
-    it("It should greet a person first name in Isizulu", function(){
+    });
+    it("It should greet my name in Isizulu", function () {
         var input = GreetFactory();
         input.message("Joseph")
         input.greetInput("IsiZulu", "Joseph")
-        assert.equal( input.message(), "Sawubona, Joseph");
-         });
-    it("It should greet a person first name in IsiXhosa", function(){
+        assert.equal(input.message(), "Sawubona, Joseph");
+    });
+    it("It should greet my name in IsiXhosa", function () {
         var input = GreetFactory();
         input.message("Thando")
         input.greetInput("IsiXhosa", "Thando")
         assert.equal(input.message(), "Molo, Thando");
-         });
-         it("It does not take a empty strings", function(){
-            var input = GreetFactory();
-            input.message("")
-            input.greetInput()
-            assert.equal(input.message(), "please enter the firstname or select a languege");
-             });
-             it("It should make the name Jason an upperCase", function(){
-
-                var input =  GreetFactory();
-                input.message("jason")
-                input.greetInput("IsiXhosa", "Jason")
-                
-                assert.equal( input.message(),"Molo, Jason")
-                 });
-                 it("It should count the Names", function(){
-
-                    var input = GreetFactory();
-                  input.greetInput("English", "Jason");
-                  input.greetInput("English", "John");
-                  input.greetInput("English", "Jesse");
-                  input.greetInput("English", "Daniel");
-                  assert.equal(input.counter(), 4)
-                     });
-                     it("It doesn't accept duplicate", function(){
-
-                        var input = GreetFactory();
-                      input.greetInput("English", "Jason");
-                      input.greetInput("English", "Jason");
-                      input.greetInput("English", "Jason");
-                      input.greetInput("English", "Jason");
-                      assert.equal(input.counter(), 1)
-                         });
     });
+    it("It will not return an empty string", function () {
+        var input = GreetFactory();
+        input.message("")
+        input.greetInput()
+        assert.equal(input.message(), "please enter the firstname or select a languege");
+    });
+    it("It must turn the name Jason to upperCase", function () {
+
+        var input = GreetFactory();
+        input.message("jason")
+        input.greetInput("IsiXhosa", "Jason")
+
+        assert.equal(input.message(), "Molo, Jason")
+    });
+    it("It is counting the names of people that have been entered in the textbox ", function () {
+
+        var input = GreetFactory();
+        input.greetInput("English", "Jason");
+        input.greetInput("English", "John");
+        input.greetInput("English", "Jesse");
+        input.greetInput("English", "Daniel");
+        assert.equal(input.counter(), 4)
+    });
+    it("My greetings app doesn't allow users to enter the same name twice", function () {
+
+        var input = GreetFactory();
+        input.greetInput("English", "Jason");
+        input.greetInput("English", "Jason");
+        input.greetInput("English", "Jason");
+        input.greetInput("English", "Jason");
+        assert.equal(input.counter(), 1)
+    });
+});
